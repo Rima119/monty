87c457fc0123476edb642be528f8a2830df4edb2
RIM EL FILALI <rimo1902@gmail.com>
2022-09-07 05:36:06 -0700

    monty.h file

---
+++ monty.h
@@ -0,0 +1,61 @@
+#ifndef __MONTY__
+#define __MONTY__
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+
+typedef enum Bool
+{
+	false,
+	true
+}Bool;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/* Prototypes */
+stack_t* new_stack(void);
+Bool is_empty_stack(stack_t **stack);
+void get_opcode(char *opcode, stack_t **stack, int line_number);
+void m_push(stack_t **stack, int line_number);
+void m_pall(stack_t **stack);
+
+#endif
