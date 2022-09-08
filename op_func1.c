d1ae899400fabc25c199ef1b9069b7fa87f89e58
RIM EL FILALI <rimo1902@gmail.com>
2022-09-08 11:36:56 -0700

    add functions

--- op_func1.c
+++ op_func1.c
@@ -28,14 +28,11 @@ void swap(stack_t **stack, unsigned int line_number)
 
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *pl;
-
-	if (!*stack || !(*stack)->next)
+	if (!stack || !(*stack) || !((*stack)->next))
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n = (*stack)->next->n + (*stack)->n;
-	free(*stack);
-	*stack = pl;
+	pop(stack, line_number);
 }
